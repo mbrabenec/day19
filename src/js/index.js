3efe0c9348dfe1457169393d1437721b07257695
mbrabenec <brabzzz@gmail.com>
2021-01-28 23:38:52 +0100

    ul

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 class Rating {
-  
+
   constructor(rating) {
-  
     this.rating = rating;
     this.element = this._createIt();
+    
   }
-
-  _createIt () {
+  _createIt() {
     const div = document.createElement("div");
-
     div.innerHTML = `
     <div class="rating">
     <div class="rating__value">3</div>
@@ -19,35 +17,36 @@ class Rating {
       <div class="rating__star"></div>
       <div class="rating__star"></div>
     </div>
-  </div>
-    `
-
-
-    Array.from(div.querySelectorAll(".rating__star"))
-      .forEach((star, index) => {
-      star.addEventListener("click", this.update(index +1))
+  </div>`
+  Array.from(div.querySelectorAll(".rating__star"))
+    .forEach((star, index) => {
+    star.addEventListener("click", this.updateAll(index + 1));
+    if(index < this.rating ) {
+      star.classList.add("rating__star--on");
+    } else {
+      star.classList.remove("rating__star--on")
+    }
     });
 
-    return div;
-  }
+    div.querySelector(".rating__value").textContent = this.rating;
+  
+  return div;
+}
 
-  update(index) {
-    this.element.querySelector(".rating__value")
-  }
-
-
-
-
-
+  updateAll (score) {
+    let a = console.log(this.element);
+  }                                                     
 
   renderTo(elem) {
     elem.appendChild(this.element);
   }
-
 }
 
-const m1 = new Rating(4);
+const m1 = new Rating(2);
 m1.renderTo(document.body);
+
+
+
 
 
 
@@ -99,3 +98,26 @@ m1.renderTo(document.body);
 // }
 
 
+
+
+
+
+document.querySelector("button").addEventListener("click", getJoke)
+const output = document.querySelector(".chuck");
+ 
+function getJoke() {
+
+    const url = 'https://api.chucknorris.io/jokes/random'
+
+    fetch(url)
+        .then((result) => 
+            result.json())
+        .then((data) => {
+
+            output.textContent = (data['value']);
+
+});
+
+}
+
+getJoke();
